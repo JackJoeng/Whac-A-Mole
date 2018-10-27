d770932cd138fe92a4a598192092bb88ac290135
Jack Yang <jackjoeng@gmail.com>
2018-10-27 16:10:10 -0400

    Updates

---
+++ precache-manifest.253e91b32a344d8cd42a0ea8a25d76f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68d8400fa59943fdf989",
+    "url": "/Whac-A-Mole/static/js/runtime~main.68d8400f.js"
+  },
+  {
+    "revision": "631e9bed4af359b775b7",
+    "url": "/Whac-A-Mole/static/js/main.631e9bed.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/Whac-A-Mole/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "631e9bed4af359b775b7",
+    "url": "/Whac-A-Mole/static/css/main.ce021a5f.chunk.css"
+  },
+  {
+    "revision": "4e0ba820f65adb165d9ccc3d3dcc0a84",
+    "url": "/Whac-A-Mole/index.html"
+  }
+];
